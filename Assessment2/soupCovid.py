7c98d3a26e0c5144df0dd715115e439e18568d89
hechar788 <149678618+hechar788@users.noreply.github.com>
2024-07-24 21:35:57 +1200

    Add files via upload

--- Assessment2/soupCovid.py
+++ Assessment2/soupCovid.py
@@ -8,25 +8,26 @@ soup = BeautifulSoup(website_content,'html.parser')
 table = soup.find('tbody')
 
 table_data = []
+new_cases_data = []
 for row in table.findAll('tr'):
     row_data = [cell.text for cell in row.findAll('td')]
     if(row_data):
-        print(len(row_data)) 
         data_item = {"id": row_data[0],
                      "country": row_data[1],
                      "totalCases": row_data[2],
                      "newCases": row_data[3],
                      "totalDeaths": row_data[4],
-                     "TotalRecovered": row_data[5],
-                     "ActiveCases": row_data[6],
-                     "CriticalCases": row_data[7],
-                     "Totcase1M": row_data[8],
-                     "Totdeath1M": row_data[9],
-                     "TotalTests": row_data[10],
-                     "Tottest1M": row_data[11]
+                     "newDeaths": row_data[4],
+                     "totalRecovered": row_data[6],
+                     "activeCases": row_data[7],
+                     "criticalCases": row_data[8],
+                     "totCase1M": row_data[9],
+                     "totDeath1M": row_data[10],
+                     "totalTests": row_data[11],
+                     "totTest1M": row_data[12]
                     }
+        new_cases_data.append(row_data[6])
         table_data.append(data_item)
 
-valid_data = [{'id': x['id'], 'country': x['country']} for x in table_data[8:]]
-sortedCovidData = sorted(valid_data, key=lambda d: d['country'])
+sortedCovidData = sorted([{'id': x['id'], 'country': x['country'], 'totalCases': x['totalCases']} for x in table_data[8:]], key=lambda d: d['country'])
 print(sortedCovidData)
