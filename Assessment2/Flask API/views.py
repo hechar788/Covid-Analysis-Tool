0c378b4c38429d2e1e816056866020aa9901c28e
hechar788 <149678618+hechar788@users.noreply.github.com>
2024-07-25 18:51:47 +1200

    Updated Model
    Synchronized the data aggregation scripts into one data model which is a lot shorter and significantly reduces code duplication

--- Assessment2/Flask API/views.py
+++ Assessment2/Flask API/views.py
@@ -1,12 +1,16 @@
 from flask import Blueprint
-from CountryData import sortedCountryData
-from CovidData import sortedCovidData
+from model import synchronizedCountryCovidData, sortedCovidData, sortedCountryData
 
 views = Blueprint(__name__, 'views')
 
 @views.route('/')
 def home():
-    return 'This is the Flask Web-API which manages the collection and covid/country data preparation for my project'
+    return '''
+    This is the Flask Web-API which manages the collection and covid/country data preparation for my project
+        <br><br>
+        <a href="/covid">Covid Stats</a><br>
+        <a href="/country">Countries</a>
+'''
 
 @views.route('/covid')
 def covid():
@@ -15,3 +19,7 @@ def covid():
 @views.route('/country')
 def country():
     return sortedCountryData
+
+@views.route('/synchronized')
+def synchronized():
+    return synchronizedCountryCovidData
