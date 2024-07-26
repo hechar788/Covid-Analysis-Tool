6765e9114520330d9c48de69b4c0d5bd1c0f3c12
hechar788 <149678618+hechar788@users.noreply.github.com>
2024-07-26 15:33:28 +1200

    Restructuring directory

--- Assessment2/Backend Data Aggregation/Lambda_Environment (LIVE)/lambda_function.py
+++ Assessment2/Backend Data Aggregation/Lambda_Environment (LIVE)/lambda_function.py
@@ -1,5 +1,9 @@
 from model import synchronizedCountryCovidData, sortedCovidData, sortedCountryData
 
+# sortedCovidData & sortedCountryData - 2 datasets collected from html parsing
+# the synchronizedCountryCovidData is the 2 datasets mapped together on their country key
+
+#print(synchronizedCountryCovidData)
 
 def lambda_handler(event, context):
     print(event)
